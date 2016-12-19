0d30f7dcc56538d3607bf4fc8aa01819fd7ba254
Sophia Gold <sophiagoldnyc@gmail.com>
2016-12-18 19:03:49 -0500

    better naming conventions

--- Regex.hs
+++ Regex.hs
@@ -4,20 +4,20 @@ import System.IO
 import System.Environment
 
 match :: (String,String) -> Bool
-match ('^':xs,y:ys) = matchHere(xs,y:ys)
-match (x:xs,y:ys) = matchHere(x:xs,y:ys)
+match ('^':regex,t:test) = matchHere(regex,t:test)
+match (r:regex,t:test) = matchHere(r:regex,t:test)
     
 matchHere :: (String,String) -> Bool
 matchHere ([],_) = True
-matchHere (x:'*':xs,y:ys) = matchStar(x,xs,y:ys)
-matchHere (['$'],y) = y == []
+matchHere (r:'*':regex,t:test) = matchStar(r,regex,t:test)
+matchHere (['$'],test) = test == []
 matchHere (_,[]) = False
-matchHere (x:xs,y:ys) | x == '.' || x == y = matchHere(xs,ys)
-                      | otherwise = matchHere(x:xs,ys)
+matchHere (r:regex,t:test) | r == '.' || r == t = matchHere(regex,test)
+                      | otherwise = matchHere(r:regex,test)
                       
 matchStar :: (Char,String,String) -> Bool
-matchStar (_,x,y) | matchHere(x,y) = True
-matchStar (c,x:xs,y:y1:ys) | c == y1 || c == '.' = matchStar(c,xs,y1:ys)
+matchStar (_,r,t) | matchHere(r,t) = True
+matchStar (c,r:regex,t:t1:test) | c == t1 || c == '.' = matchStar(c,regex,t1:test)
 
 main :: IO ()
 main = do
